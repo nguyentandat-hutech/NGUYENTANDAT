24fa07d4eae733c65b4a25edd1c72c56eb28ef87
nguyentandat-hutech <nguyentandat130204@gmail.com>
2026-03-19 10:23:04 +0700

    Initial commit: Spring Boot project with Docker support

---
+++ src/main/java/com/example/nguyentandat/NguyentandatApplication.java
@@ -0,0 +1,13 @@
+package com.example.nguyentandat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyentandatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyentandatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nguyentandat/NguyentandatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nguyentandat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyentandatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
